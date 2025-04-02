8f501e1353d3fd31c9f4ef3abd8ceb7441706c5a
0xsj <sjtommylee@gmail.com>
2025-04-01 23:58:31 -0700

    config

---
+++ api/server.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	mux.Router
+}
+
+func NewServer() *Server {
+	s := &Server{}
+	s.Router = *mux.NewRouter()
+	return s
+}
+
+func (s *Server) Run(address string) error {
+	return http.ListenAndServe(address, s)
+}
+
+func main(){
+	s := NewServer()
+	s.HandleFunc("/health", s.HealthCheckHandler).Methods("GET")
+	api := s.PathPrefix("/api").Subrouter()
+	users := api.PathPrefix("/users").Subrouter()
+	users.HandleFunc("/create", s.CreateUser).Methods("POST")
+	s.Run(":8080")
+}
+
+func (s * Server) HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, `${"alive": true}`)
+}
+
+func (s *Server) CreateUser(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Environment string `mapstructure:"ENVIRONMENT"`
+	Host		string `mapstructure:"HOST"`
+	Port		string `mapstructure:"PORT"`
+
+	DBUsername	string `mapstructure:"DB_USERNAME"`
+	DBPassword	string `mapstructure:"DB_PASSWORD"`
+	DBHost		string `mapstructure:"DB_HOSTNAME"`
+	DBPort		string `mapstructure:"DB_PORT"`
+	DBName		string `mapstructure:"DB_NAME"`
+}
+
+func LoadConfig(name string, path string) (config Config) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName(name)
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("config: %v", err)
+		return
+	}
+	if err := viper.Unmarshal(&config); err != nil {
+		log.Fatalf("config: %v", err)
+		return
+	}
+	return
+}
