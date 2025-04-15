f4b5aa0705fdb4ef73cf5839cead1077b6bf2bea
0xsj <sjtommylee@gmail.com>
2025-04-15 01:53:31 -0700

    create user repository

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -2,29 +2,87 @@ package repository
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 
 	db "github.com/0xsj/gin-sqlc/db/sqlc"
 	"github.com/google/uuid"
+	"github.com/jackc/pgconn"
 )
 
+var (
+	ErrRecordNotFound = errors.New("record not found")
+	ErrDuplicateKey   = errors.New("duplicate key violation")
+	ErrDatabase       = errors.New("database error")
+)
 
 type UserRepository interface {
-	CreateUser(ctx context.Context, arg CreateUserParams) (db.User, error)
-	GetUser(ctx context.Context, userID uuid.UUID) (db.User, error)
-	GetUserByUsername(ctx context.Context, username string) (db.User, error)
-	GetUserByEmail(ctx context.Context, email string) (db.User, error)
-	UpdateUser(ctx context.Context, arg UpdateUserParams) error
-	UpdateUsername(ctx context.Context, userID uuid.UUID, username string) error
-	UpdateEmail(ctx context.Context, userID uuid.UUID, email string) error
-	UpdatePremiumStatus(ctx context.Context, userID uuid.UUID, isPremium bool) error
-	DeleteUser(ctx context.Context, userID uuid.UUID) error
+	CreateUser(ctx context.Context, arg CreateUserParams) (*db.User, error)
+	// GetUser(ctx context.Context, userID uuid.UUID) (db.User, error)
+	// GetUserByUsername(ctx context.Context, username string) (db.User, error)
+	// GetUserByEmail(ctx context.Context, email string) (db.User, error)
+	// UpdateUser(ctx context.Context, arg UpdateUserParams) error
+	// UpdateUsername(ctx context.Context, userID uuid.UUID, username string) error
+	// UpdateEmail(ctx context.Context, userID uuid.UUID, email string) error
+	// UpdatePremiumStatus(ctx context.Context, userID uuid.UUID, isPremium bool) error
+	// DeleteUser(ctx context.Context, userID uuid.UUID) error
 }
 
-type CreateUserParams struct {}
+type CreateUserParams struct {
+	Username		string 
+	Email			string
+	FirstName		string
+	LastName		string
+	ProfileImageURL	string 
+	Bio				string
+	Theme			string
+}
 
-type UpdateUserParams struct {}
+type UpdateUserParams struct {
+	UserID          uuid.UUID
+	FirstName       string
+	LastName        string
+	ProfileImageURL string
+	Bio             string
+	Theme           string
+}
 
 type SQLCUserRepository struct {
 	db *db.Queries
 }
 
+func NewUserRepository(db *db.Queries) UserRepository {
+	return &SQLCUserRepository{
+		db: db,
+	}
+}
+
+func (r *SQLCUserRepository) CreateUser(ctx context.Context, arg CreateUserParams) (*db.User, error) {
+	firstNamePtr := sql.NullString{String: arg.FirstName, Valid: arg.FirstName != ""}
+	lastNamePtr := sql.NullString{String: arg.LastName, Valid: arg.LastName != ""}
+	profileImagePtr := sql.NullString{String: arg.ProfileImageURL, Valid: arg.ProfileImageURL != ""}
+	bioPtr := sql.NullString{String: arg.Bio, Valid: arg.Bio != ""}
+	themePtr := sql.NullString{String: arg.Theme, Valid: arg.Theme != ""}
+	
+	params := db.CreateUserParams{
+		Username:        arg.Username,
+		Email:           arg.Email,
+		FirstName:       &firstNamePtr.String,
+		LastName:        &lastNamePtr.String,
+		ProfileImageUrl: &profileImagePtr.String,
+		Bio:             &bioPtr.String,
+		Theme:           &themePtr.String,
+	}
+
+	user, err := r.db.CreateUser(ctx, params)
+	if err != nil {
+		pgErr, ok := err.(*pgconn.PgError)
+		if ok {
+			if pgErr.Code == "23505" {
+				return nil, ErrDuplicateKey
+			}
+		}
+		return nil, ErrDatabase
+	}
+	return user, nil
+}
