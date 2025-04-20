27761ce6f219891d12f1de6a2375d3a75d95d79f
0xsj <sjtommylee@gmail.com>
2025-04-20 01:55:37 -0700

    stub out services

--- service/user_service.go
+++ service/user_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"context"
 	"regexp"
 	"strings"
 
@@ -9,36 +10,63 @@ import (
 )
 
 type UserService interface {
-	// CreateUser(ctx context.Context, input CreateUserInput) (*UserDTO, error)
-	// GetUser(ctx context.Context, id string) (*UserDTO, error)
-	// GetUserByUsername(ctx context.Context, username string) (*UserDTO, error)
-	// GetUserByEmail(ctx context.Context, email string) (*UserDTO, error)
-	// UpdateUser(ctx context.Context, id string, input UpdateUserInput) (*UserDTO, error)
-	// DeleteUser(ctx context.Context, id string) error
+	CreateUser(ctx context.Context, input CreateUserInput) (*UserDTO, error)
+	GetUser(ctx context.Context, id string) (*UserDTO, error)
+	GetUserByUsername(ctx context.Context, username string) (*UserDTO, error)
+	GetUserByHandle(ctx context.Context, handle string) (*UserDTO, error)
+	GetUserByEmail(ctx context.Context, email string) (*UserDTO, error)
+	UpdateUser(ctx context.Context, id string, input UpdateUserInput) (*UserDTO, error)
+	UpdateHandle(ctx context.Context, id string, handle string) (*UserDTO, error)
+	UpdatePremiumStatus(ctx context.Context, id string, isPremium bool) (*UserDTO, error)
+	UpdateAdminStatus(ctx context.Context, id string, isAdmin bool) (*UserDTO, error)
+	UpdateOnboardedStatus(ctx context.Context, id string, onboarded bool) (*UserDTO, error)
+	DeleteUser(ctx context.Context, id string) error
 }
 
 type CreateUserInput struct {
-	Username  string `json:"username" binding:"required"`
-	Email     string `json:"email" binding:"required"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	Username        string `json:"username" binding:"required"`
+	Handle          string `json:"handle" binding:"required"`
+	Email           string `json:"email" binding:"required"`
+	FirstName       string `json:"first_name"`
+	LastName        string `json:"last_name"`
+	Bio             string `json:"bio"`
+	ProfileImageURL string `json:"profile_image_url"`
+	LayoutVersion   string `json:"layout_version"`
+	CustomDomain    string `json:"custom_domain"`
+	IsPremium       bool   `json:"is_premium"`
+	IsAdmin         bool   `json:"is_admin"`
+	Onboarded       bool   `json:"onboarded"`
 }
 
 type UpdateUserInput struct {
-    Username  *string `json:"username"`
-    Email     *string `json:"email"`
-    FirstName *string `json:"first_name"`
-    LastName  *string `json:"last_name"`
+	Username        *string `json:"username"`
+	Email           *string `json:"email"`
+	FirstName       *string `json:"first_name"`
+	LastName        *string `json:"last_name"`
+	Bio             *string `json:"bio"`
+	ProfileImageURL *string `json:"profile_image_url"`
+	LayoutVersion   *string `json:"layout_version"`
+	CustomDomain    *string `json:"custom_domain"`
 }
 
 type UserDTO struct {
-	ID        string `json:"id"`
-	Username  string `json:"username"`
-	Email     string `json:"email"`
-	FirstName string `json:"first_name,omitempty"`
-	LastName  string `json:"last_name,omitempty"`
-	IsPremium bool   `json:"is_premium"`
+	ID              string `json:"id"`
+	Username        string `json:"username"`
+	Handle          string `json:"handle,omitempty"`
+	Email           string `json:"email"`
+	FirstName       string `json:"first_name,omitempty"`
+	LastName        string `json:"last_name,omitempty"`
+	Bio             string `json:"bio,omitempty"`
+	ProfileImageURL string `json:"profile_image_url,omitempty"`
+	LayoutVersion   string `json:"layout_version,omitempty"`
+	CustomDomain    string `json:"custom_domain,omitempty"`
+	IsPremium       bool   `json:"is_premium"`
+	IsAdmin         bool   `json:"is_admin"`
+	Onboarded       bool   `json:"onboarded"`
+	CreatedAt       string `json:"created_at,omitempty"`
+	UpdatedAt       string `json:"updated_at,omitempty"`
 }
+
 
 type userService struct {
 	userRepo repository.UserRepository
@@ -50,152 +78,38 @@ func NewUserService(userRepo repository.UserRepository) UserService {
 	}
 }
 
-// func (s *userService) CreateUser(ctx context.Context, input CreateUserInput) (*UserDTO, error) {
-// 	fmt.Println("UserService.CreateUser called")
 
-// 	if !isValidUsername(input.Username) {
-// 		return nil, api.ErrInvalidInput
-// 	}
-
-// 	if !isValidEmail(input.Email) {
-// 		return nil, api.ErrInvalidInput
-// 	}
-
-// 	params := repository.CreateUserParams{
-// 		Username:  input.Username,
-// 		Email:     input.Email,
-// 		FirstName: input.FirstName,
-// 		LastName:  input.LastName,
-// 	}
-
-// 	fmt.Printf("Calling repository with params: %+v\n", params)
-
-// 	user, err := s.userRepo.CreateUser(ctx, params)
-// 	if err != nil {
-// 		fmt.Printf("Repository error: %v\n", err)
-// 		if err == repository.ErrDuplicateKey {
-// 			return nil, api.ErrDuplicateEntry
-// 		}
-// 		return nil, api.ErrInternalServer
-// 	}
-
-// 	return mapUserToDTO(user), nil
-// }
-
-// func (s *userService) GetUser(ctx context.Context, id string) (*UserDTO, error) {
-// 	userID, err := uuid.Parse(id)
-// 	if err != nil {
-// 		return nil, api.ErrInvalidInput
-// 	}
-
-// 	user, err := s.userRepo.GetUser(ctx, userID)
-// 	if err != nil {
-// 		if errors.Is(err, repository.ErrRecordNotFound) {
-// 			return nil, api.ErrNotFound
-// 		}
-// 		return nil, api.ErrInternalServer
-// 	}
-
-// 	return mapUserToDTO(user), nil
-// }
-
-// func (s *userService) GetUserByUsername(ctx context.Context, username string) (*UserDTO, error) {
-// 	user, err := s.userRepo.GetUserByUsername(ctx, username)
-// 	if err != nil {
-// 		if errors.Is(err, repository.ErrRecordNotFound) {
-// 			return nil, api.ErrNotFound
-// 		}
-// 		return nil, api.ErrInternalServer
-// 	}
-
-// 	return mapUserToDTO(user), nil
-// }
+func (s *userService) CreateUser(ctx context.Context, input CreateUserInput) (*UserDTO, error) {}
 
 
-// func (s *userService) GetUserByEmail(ctx context.Context, email string) (*UserDTO, error) {
-// 	user, err := s.userRepo.GetUserByEmail(ctx, email)
-// 	if err != nil {
-// 		if errors.Is(err, repository.ErrRecordNotFound) {
-// 			return nil, api.ErrNotFound
-// 		}
-// 		return nil, api.ErrInternalServer
-// 	}
+func (s *userService) GetUser(ctx context.Context, id string) (*UserDTO, error) {}
 
-// 	return mapUserToDTO(user), nil
-// }
 
-// func (s *userService) UpdateUser(ctx context.Context, id string, input UpdateUserInput) (*UserDTO, error) {
-//     userID, err := uuid.Parse(id)
-//     if err != nil {
-//         return nil, api.ErrInvalidInput
-//     }
-    
-//     if input.Username != nil && !isValidUsername(*input.Username) {
-//         return nil, api.ErrInvalidInput
-//     }
-    
-//     if input.Email != nil && !isValidEmail(*input.Email) {
-//         return nil, api.ErrInvalidInput
-//     }
-    
-//     params := repository.UpdateUserParams{
-//         UserID: userID,
-//     }
-    
-//     if input.Username != nil {
-//         params.Username = *input.Username
-//     }
-    
-//     if input.Email != nil {
-//         params.Email = *input.Email
-//     }
-    
-//     if input.FirstName != nil {
-//         params.FirstName = *input.FirstName
-//     }
-    
-//     if input.LastName != nil {
-//         params.LastName = *input.LastName
-//     }
-    
-    
-//     err = s.userRepo.UpdateUser(ctx, params)
-//     if err != nil {
-//         if err == repository.ErrDuplicateKey {
-//             return nil, api.ErrDuplicateEntry
-//         }
-//         return nil, api.ErrInternalServer
-//     }
-    
-//     updatedUser, err := s.userRepo.GetUser(ctx, userID)
-//     if err != nil {
-//         return nil, api.ErrInternalServer
-//     }
-    
-//     return mapUserToDTO(updatedUser), nil
-// }
+func (s *userService) GetUserByUsername(ctx context.Context, username string) (*UserDTO, error) {}
 
-// func (s *userService) DeleteUser(ctx context.Context, id string) error {
-// 	userID, err := uuid.Parse(id)
-// 	if err != nil {
-// 		return api.ErrInvalidInput
-// 	}
 
-// 	_, err = s.userRepo.GetUser(ctx, userID)
-// 	if err != nil {
-// 		if errors.Is(err, repository.ErrRecordNotFound) {
-// 			return api.ErrNotFound
-// 		}
-// 		return api.ErrInternalServer
-// 	}
+func (s *userService) GetUserByHandle(ctx context.Context, handle string) (*UserDTO, error) {}
 
-// 	err = s.userRepo.DeleteUser(ctx, userID)
-// 	if err != nil {
-// 		return api.ErrInternalServer
-// 	}
 
-// 	return nil
-// }
+func (s *userService) GetUserByEmail(ctx context.Context, email string) (*UserDTO, error) {}
+
+
+func (s *userService) UpdateUser(ctx context.Context, id string, input UpdateUserInput) (*UserDTO, error) {}
+
+
+func (s *userService) UpdateHandle(ctxt context.Context, id string, handle string) (*UserDTO, error) {}
+
+
+func (s *userService) UpdatePremiumStatus(ctx context.Context, id string, isPremium bool) (*UserDTO, error) {}
+
+
+func (s *userService) UpdateAdminStatus(ctx context.Context, id string, isAdmin bool) (*UserDTO, error) {}
+
+
+func (s *userService) UpdateOnboardedStatus(ctx context.Context, id string, onboarded bool) (*UserDTO, error) {}
+
+
+func (s *userService) DeleteUser(ctx context.Context, id string) error {}
 
 
 func mapUserToDTO(user *db.User) *UserDTO {
@@ -204,13 +118,44 @@ func mapUserToDTO(user *db.User) *UserDTO {
 		Username:  user.Username,
 		Email:     user.Email,
 		IsPremium: user.IsPremium != nil && *user.IsPremium,
+		IsAdmin:   user.IsAdmin != nil && *user.IsAdmin,
+		Onboarded: user.Onboarded != nil && *user.Onboarded,
 	}
 
+	if user.Handle != nil {
+		dto.Handle = *user.Handle
+	}
+	
 	if user.FirstName != nil {
 		dto.FirstName = *user.FirstName
 	}
+	
 	if user.LastName != nil {
 		dto.LastName = *user.LastName
+	}
+	
+	if user.Bio != nil {
+		dto.Bio = *user.Bio
+	}
+	
+	if user.ProfileImageUrl != nil {
+		dto.ProfileImageURL = *user.ProfileImageUrl
+	}
+	
+	if user.LayoutVersion != nil {
+		dto.LayoutVersion = *user.LayoutVersion
+	}
+	
+	if user.CustomDomain != nil {
+		dto.CustomDomain = *user.CustomDomain
+	}
+	
+	if user.CreatedAt != nil {
+		dto.CreatedAt = user.CreatedAt.String()
+	}
+	
+	if user.UpdatedAt != nil {
+		dto.UpdatedAt = user.UpdatedAt.String()
 	}
 
 	return dto
@@ -226,10 +171,19 @@ func isValidUsername(username string) bool {
 	return match
 }
 
+func isValidHandle(handle string) bool {
+	if len(handle) < 2 || len(handle) > 30 {
+		return false
+	}
+
+	pattern := `^[a-zA-Z0-9_-]+$`
+	match, _ := regexp.MatchString(pattern, handle)
+	return match
+}
+
 func isValidEmail(email string) bool {
 	email = strings.TrimSpace(email)
 	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
 	match, _ := regexp.MatchString(pattern, email)
 	return match
 }
-
