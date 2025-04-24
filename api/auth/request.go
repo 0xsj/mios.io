dfc0e90f38cede8fcd23c1c17e86e1759bc08c18
0xsj <sjtommylee@gmail.com>
2025-04-23 22:49:09 -0700

    structure for request, handlers, added dto

---
+++ api/auth/request.go
@@ -0,0 +1,11 @@
+package auth
+
+type RegisterRequest struct {}
+
+type LoginRequest struct {}
+
+type RefreshTokenRequest struct {}
+
+type ForgotPasswordRequest struct {}
+
+type RestPasswordRequest struct {}
