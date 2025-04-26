40647a00e9dfd38ce9033a1662486c38a38a6c7f
0xsj <sjtommylee@gmail.com>
2025-04-26 13:10:52 -0700

    content service

--- repository/content_repository.go
+++ repository/content_repository.go
@@ -5,6 +5,7 @@ import (
 
 	db "github.com/0xsj/gin-sqlc/db/sqlc"
 	"github.com/google/uuid"
+	"github.com/jackc/pgconn"
 	"github.com/jackc/pgtype"
 )
 
@@ -71,14 +72,184 @@ func NewContentRepository(db *db.Queries) ContentRepository {
 	}
 }
 
-func (r *SQLContentRepository) CreateContentItem(ctx context.Context, params CreateContentItemParams)(*db.ContentItem, error){}
+func (r *SQLContentRepository) CreateContentItem(ctx context.Context, params CreateContentItemParams)(*db.ContentItem, error){
+	var title, href, url, mediaType, desktopStyle, mobileStyle, halign, valign *string
+    var desktopX, desktopY, mobileX, mobileY *int32
+    
+	if params.Title != nil {
+		title = params.Title
+	}
 
-func (r *SQLContentRepository) GetContentItem(ctx context.Context, itemID uuid.UUID) (*db.ContentItem, error) {}
+	if params.Href != nil {
+		href = params.Href	
+	}
 
-func (r *SQLContentRepository) GetUserContentItems(ctx context.Context, userID uuid.UUID)  ([]*db.ContentItem, error) {}
+	if params.URL != nil {
+		url = params.URL
+	}
+
+	if params.MediaType != nil {
+		mediaType = params.MediaType	
+	}
+
+	if params.DesktopX != nil {
+		desktopX = params.DesktopX
+	}
+
+	if params.DesktopY != nil {
+		desktopY = params.DesktopY
+	}
+
+	if params.MobileX != nil {
+		mobileX = params.MobileX
+	}
+
+	if params.MobileY != nil {
+		mobileY = params.MobileY
+	}
+
+	if params.MobileStyle != nil {
+		mobileStyle = params.MobileStyle
+	}
+
+	if params.HAlign != nil {
+		halign = params.HAlign
+	}
+
+	if params.VAlign != nil {
+		valign = params.VAlign
+	}
+
+	isActive := &params.IsActive
+
+	sqlcParams := db.CreateContentItemParams{
+		UserID:      params.UserID,
+        ContentID:   params.ContentType,
+        ContentType: params.ContentType,
+        Title:       title,
+        Href:        href,
+        Url:         url,
+        MediaType:   mediaType,
+        DesktopX:    desktopX,
+        DesktopY:    desktopY,
+        DesktopStyle: desktopStyle,
+        MobileX:     mobileX,
+        MobileY:     mobileY,
+        MobileStyle: mobileStyle,
+        Halign:      halign,
+        Valign:      valign,
+        ContentData: params.ContentData,
+        Overrides:   params.Overrides,
+        IsActive:    isActive,
+	}
+
+	item, err := r.db.CreateContentItem(ctx, sqlcParams)
+	if err != nil {
+		pgErr, ok := err.(*pgconn.PgError)
+		if ok {
+			if pgErr.Code == "23505" {
+				return nil, ErrDuplicateKey
+			}
+
+			if pgErr.Code == "23503" {
+				return nil, ErrForeignKeyViolation
+			}
+		}
+
+		return nil, ErrDatabase
+	}
+
+	return item, nil
+}
+
+func (r *SQLContentRepository) GetContentItem(ctx context.Context, itemID uuid.UUID) (*db.ContentItem, error) {
+	item, err := r.db.GetContentItem(ctx, itemID)
+	if err != nil {
+		return nil, ErrRecordNotFound
+	}
+
+	return item, nil
+}
+
+func (r *SQLContentRepository) GetUserContentItems(ctx context.Context, userID uuid.UUID)  ([]*db.ContentItem, error) {
+	items, err := r.db.GetUserContentItems(ctx, userID)
+	if err != nil {
+		return nil, ErrDatabase
+	}
+
+	return items, nil
+}
 
 
-func (r *SQLContentRepository) UpdateContentItem(ctx context.Context,params UpdateContentItemParams) error {}
+func (r *SQLContentRepository) UpdateContentItem(ctx context.Context, params UpdateContentItemParams) error {
+    // Convert our params to SQLC params
+    var title, href, url, mediaType, desktopStyle, mobileStyle, halign, valign *string
+    var isActive *bool
+    
+    if params.Title != nil {
+        title = params.Title
+    }
+    if params.Href != nil {
+        href = params.Href
+    }
+    if params.URL != nil {
+        url = params.URL
+    }
+    if params.MediaType != nil {
+        mediaType = params.MediaType
+    }
+    if params.DesktopStyle != nil {
+        desktopStyle = params.DesktopStyle
+    }
+    if params.MobileStyle != nil {
+        mobileStyle = params.MobileStyle
+    }
+    if params.HAlign != nil {
+        halign = params.HAlign
+    }
+    if params.VAlign != nil {
+        valign = params.VAlign
+    }
+    if params.IsActive != nil {
+        isActive = params.IsActive
+    }
+    
+    // Initialize ContentData and Overrides as null by default
+    contentData := pgtype.JSONB{Status: pgtype.Null}
+    overrides := pgtype.JSONB{Status: pgtype.Null}
+    
+    // If we have ContentData, use it
+    if params.ContentData != nil {
+        contentData = *params.ContentData
+    }
+    
+    // If we have Overrides, use it
+    if params.Overrides != nil {
+        overrides = *params.Overrides
+    }
+    
+    sqlcParams := db.UpdateContentItemParams{
+        ItemID:      params.ItemID,
+        Title:       title,
+        Href:        href,
+        Url:         url,
+        MediaType:   mediaType,
+        DesktopStyle: desktopStyle,
+        MobileStyle: mobileStyle,
+        Halign:      halign,
+        Valign:      valign,
+        ContentData: contentData,
+        Overrides:   overrides,
+        IsActive:    isActive,
+    }
+    
+    err := r.db.UpdateContentItem(ctx, sqlcParams)
+    if err != nil {
+        return ErrDatabase
+    }
+    
+    return nil
+}
 
 
 func (r *SQLContentRepository) UpdateContentItemPosition(ctx context.Context, params UpdatePositionParams) error {
--- service/content_service.go
+++ service/content_service.go
@@ -2,8 +2,15 @@ package service
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"time"
 
+	"github.com/0xsj/gin-sqlc/api"
+	db "github.com/0xsj/gin-sqlc/db/sqlc"
 	"github.com/0xsj/gin-sqlc/repository"
+	"github.com/google/uuid"
+	"github.com/jackc/pgtype"
 )
 
 type ContentService interface {
@@ -104,16 +111,339 @@ func NewContentService(contentRepo repository.ContentRepository, userRepo reposi
 	}
 }
 
-func (s *contentService) CreateContentItem(){}
+func (s *contentService) CreateContentItem(ctx context.Context, input CreateContentItemInput) (*ContentItemDTO, error){
+	userID, err := uuid.Parse(input.UserID)
+	if err != nil {
+		return nil, api.ErrInvalidInput
+	}
 
-func (s *contentService) GetContentItem() {}
+	_, err = s.userRepo.GetUser(ctx, userID)
+	if err != nil {
+		if errors.Is(err, repository.ErrRecordNotFound) {
+			return nil, api.ErrNotFound
+		}
+		return nil, api.ErrInternalServer
+	}
 
-func (s *contentService) GetUserContentItems() {}
+	var contentData, overrides pgtype.JSONB
+	if len(input.ContentData) > 0 {
+		contentData.Status = pgtype.Present
+		contentData.Bytes, err = json.Marshal(input.ContentData)
+		if err != nil {
+			return nil, api.ErrInvalidInput
+		}
+	} else {
+		contentData.Status = pgtype.Null
+	}
 
-func (s *contentService) UpdateContentItem() {}
+	if len(input.Overrides) > 0 {
+		overrides.Status = pgtype.Present
+		overrides.Bytes , err = json.Marshal(input.Overrides)
+		if err != nil {
+			return nil, api.ErrInvalidInput
+		}
+	} else {
+		overrides.Status = pgtype.Null
+	}
 
-func (s *contentService) UpdateContentItemPosition() {}
+	params := repository.CreateContentItemParams{
+		UserID:       userID,
+		ContentID:    input.ContentID,
+		ContentType:  input.ContentType,
+		Title:        input.Title,
+		Href:         input.Href,
+		URL:          input.URL,
+		MediaType:    input.MediaType,
+		DesktopX:     input.DesktopX,
+		DesktopY:     input.DesktopY,
+		DesktopStyle: input.DesktopStyle,
+		MobileX:      input.MobileX,
+		MobileY:      input.MobileY,
+		MobileStyle:  input.MobileStyle,
+		HAlign:       input.HAlign,
+		VAlign:       input.VAlign,
+		ContentData:  contentData,
+		Overrides:    overrides,
+		IsActive:     true, 
+	}	
+	
+	contentItem, err := s.contentRepo.CreateContentItem(ctx, params)
+	if err != nil {
+		if errors.Is(err, repository.ErrDuplicateKey) {
+			return nil, api.ErrDuplicateEntry
+		}
+		return nil, api.ErrInternalServer
+	}
+	return mapContentItemToDTO(contentItem), nil
+}
 
-func (s *contentService) DeleteContentItem() {}
+func (s *contentService) GetContentItem(ctx context.Context, itemIDStr string) (*ContentItemDTO, error) {
+	itemID, err := uuid.Parse(itemIDStr)
+	if err != nil {
+		return nil, api.ErrInvalidInput
+	}
 
-func mapContentItemToDTO() {}
+	contentItem, err := s.contentRepo.GetContentItem(ctx, itemID)
+	if err != nil {
+		if errors.Is(err, repository.ErrRecordNotFound) {
+			return nil, api.ErrNotFound
+		}
+		return nil, api.ErrInternalServer
+	}
+	return mapContentItemToDTO(contentItem), nil
+}
+
+func (s *contentService) GetUserContentItems(ctx context.Context, userIDStr string)([]*ContentItemDTO, error) {
+	userID, err := uuid.Parse(userIDStr)
+	if err != nil {
+		return nil, api.ErrInvalidInput
+	}
+
+	_, err = s.userRepo.GetUser(ctx, userID)
+	if err != nil {
+		if errors.Is(err, repository.ErrRecordNotFound) {
+			return nil, api.ErrNotFound
+		}
+		return nil, api.ErrInternalServer
+	}
+
+	contentItems, err := s.contentRepo.GetUserContentItems(ctx, userID)
+	if err != nil {
+		return nil, api.ErrInternalServer
+	}
+
+	dtos := make([]*ContentItemDTO, len(contentItems))
+	for i, item := range contentItems {
+		dtos[i] = mapContentItemToDTO(item)
+	}
+
+	return dtos, nil
+
+}
+
+func (s *contentService) UpdateContentItem(ctx context.Context, itemIDStr string, input UpdateContentItemInput) (*ContentItemDTO, error) {
+    itemID, err := uuid.Parse(itemIDStr)
+    if err != nil {
+        return nil, api.ErrInvalidInput
+    }
+
+    _, err = s.contentRepo.GetContentItem(ctx, itemID)
+    if err != nil {
+        if errors.Is(err, repository.ErrRecordNotFound) {
+            return nil, api.ErrNotFound
+        }
+        return nil, api.ErrInternalServer
+    }
+
+    // Prepare the update parameters
+    var contentData, overrides *pgtype.JSONB
+    
+    if input.ContentData != nil && len(input.ContentData) > 0 {
+        var cData pgtype.JSONB
+        cData.Status = pgtype.Present
+        cData.Bytes, err = json.Marshal(input.ContentData)
+        if err != nil {
+            return nil, api.ErrInvalidInput
+        }
+        contentData = &cData
+    }
+
+    if input.Overrides != nil && len(input.Overrides) > 0 {
+        var oData pgtype.JSONB
+        oData.Status = pgtype.Present
+        oData.Bytes, err = json.Marshal(input.Overrides)
+        if err != nil {
+            return nil, api.ErrInvalidInput
+        }
+        overrides = &oData
+    }
+
+    params := repository.UpdateContentItemParams{
+        ItemID:       itemID,
+        Title:        input.Title,
+        Href:         input.Href,
+        URL:          input.URL,
+        MediaType:    input.MediaType,
+        DesktopStyle: input.DesktopStyle,
+        MobileStyle:  input.MobileStyle,
+        HAlign:       input.HAlign,
+        VAlign:       input.VAlign,
+        ContentData:  contentData,
+        Overrides:    overrides,
+        IsActive:     input.IsActive,
+    }
+
+    err = s.contentRepo.UpdateContentItem(ctx, params)
+    if err != nil {
+        return nil, api.ErrInternalServer
+    }
+
+    updatedItem, err := s.contentRepo.GetContentItem(ctx, itemID)
+    if err != nil {
+        return nil, api.ErrInternalServer
+    }
+
+    return mapContentItemToDTO(updatedItem), nil
+}
+
+func (s *contentService) UpdateContentItemPosition(ctx context.Context, itemIDStr string, input UpdatePositionInput) (*ContentItemDTO, error) {
+	itemID, err := uuid.Parse(itemIDStr)
+	if err != nil {
+		return nil, api.ErrInvalidInput
+	}
+
+	_, err = s.contentRepo.GetContentItem(ctx, itemID)
+	if err != nil {
+		if errors.Is(err, repository.ErrRecordNotFound) {
+			return nil, api.ErrNotFound
+		}
+		return nil, api.ErrInternalServer
+	}
+
+	params := repository.UpdatePositionParams{
+		ItemID:    itemID,
+		DesktopX:  input.DesktopX,
+		DesktopY:  input.DesktopY,
+		MobileX:   input.MobileX,
+		MobileY:   input.MobileY,
+	}
+
+	err = s.contentRepo.UpdateContentItemPosition(ctx, params)
+	if err != nil {
+		return nil, api.ErrInternalServer
+	}
+
+	updatedItem, err := s.contentRepo.GetContentItem(ctx, itemID)
+	if err != nil {
+		return nil, api.ErrInternalServer
+	}
+
+	return mapContentItemToDTO(updatedItem), nil
+}
+
+func (s *contentService) DeleteContentItem(ctx context.Context, itemIDStr string) error {
+	itemID, err := uuid.Parse(itemIDStr)
+	if err != nil {
+		return api.ErrInvalidInput
+	}
+
+	_, err = s.contentRepo.GetContentItem(ctx, itemID)
+	if err != nil {
+		if errors.Is(err, repository.ErrRecordNotFound) {
+			return api.ErrNotFound
+		}
+		return api.ErrInternalServer
+	}
+
+	err = s.contentRepo.DeleteContentItem(ctx, itemID)
+	if err != nil {
+		return api.ErrInternalServer
+	}
+
+	return nil
+}
+
+func mapContentItemToDTO(item *db.ContentItem) *ContentItemDTO {
+	dto := &ContentItemDTO{
+		ID:          item.ItemID.String(),
+		UserID:      item.UserID.String(),
+		ContentID:   item.ContentID,
+		ContentType: item.ContentType,
+		IsActive:    item.IsActive != nil && *item.IsActive,
+		Position: PositionDTO{
+			Desktop: struct {
+				X int32 `json:"x"`
+				Y int32 `json:"y"`
+			}{
+				X: 0,
+				Y: 0,
+			},
+			Mobile: struct {
+				X int32 `json:"x"`
+				Y int32 `json:"y"`
+			}{
+				X: 0,
+				Y: 0,
+			},
+		},
+		Style: StyleDTO{},
+	}
+
+	if item.Title != nil {
+		dto.Title = *item.Title
+	}
+
+	if item.Href != nil {
+		dto.Href = *item.Href
+	}
+
+	if item.Url != nil {
+		dto.URL = *item.Url
+	}
+
+	if item.MediaType != nil {
+		dto.MediaType = *item.MediaType
+	}
+
+	if item.DesktopX != nil {
+		dto.Position.Desktop.X = *item.DesktopX
+	}
+
+	if item.DesktopY != nil {
+		dto.Position.Desktop.Y = *item.DesktopY
+	}
+
+	if item.DesktopStyle != nil {
+		dto.Style.Desktop = *item.DesktopStyle
+	}
+
+	if item.MobileX != nil {
+		dto.Position.Mobile.X = *item.MobileX
+	}
+
+	if item.MobileY != nil {
+		dto.Position.Mobile.Y = *item.MobileY
+	}
+
+	if item.MobileStyle != nil {
+		dto.Style.Mobile = *item.MobileStyle
+	}
+
+	if item.Halign != nil || item.Valign != nil {
+		dto.HAlign = make(map[string]string)
+		dto.VAlign = make(map[string]string)
+
+		if item.Halign != nil {
+			dto.HAlign["default"] = *item.Halign
+		}
+
+		if item.Valign != nil {
+			dto.VAlign["default"] = *item.Valign
+		}
+	}
+
+	// Convert JSONB to map
+	if item.ContentData.Status == pgtype.Present {
+		var contentData map[string]interface{}
+		if err := json.Unmarshal(item.ContentData.Bytes, &contentData); err == nil {
+			dto.ContentData = contentData
+		}
+	}
+
+	if item.Overrides.Status == pgtype.Present {
+		var overrides map[string]interface{}
+		if err := json.Unmarshal(item.Overrides.Bytes, &overrides); err == nil {
+			dto.Overrides = overrides
+		}
+	}
+
+	if item.CreatedAt != nil {
+		dto.CreatedAt = item.CreatedAt.Format(time.RFC3339)
+	}
+
+	if item.UpdatedAt != nil {
+		dto.UpdatedAt = item.UpdatedAt.Format(time.RFC3339)
+	}
+
+	return dto
+}
