9a3d22e953ae4d74e6264171e7f557e7f9c79ece
0xsj <sjtommylee@gmail.com>
2025-05-04 00:04:20 -0700

    add caller info in logging

--- log/zap_logger.go
+++ log/zap_logger.go
@@ -7,61 +7,82 @@ import (
 )
 
 type ZapLogger struct {
-	logger *zap.SugaredLogger
+    logger *zap.SugaredLogger
+    layer  string // Added layer field for context
 }
 
 func NewZapLogger(environment string) Logger {
-	var config zap.Config
+    var config zap.Config
+    if environment == "production" {
+        config = zap.NewProductionConfig()
+    } else {
+        config = zap.NewDevelopmentConfig()
+        config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+    }
+    
+    // Enable caller information
+    config.DisableCaller = false
+    config.EncoderConfig.CallerKey = "caller"
+    config.EncoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
+    
+    logger, _ := config.Build(zap.AddCallerSkip(1)) // Skip one level to show actual caller
+    return &ZapLogger{
+        logger: logger.Sugar(),
+    }
+}
 
-	if environment == "production" {
-		config = zap.NewProductionConfig()
-	} else {
-		config = zap.NewDevelopmentConfig()
-		config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-	}
+// WithLayer creates a new logger with layer information
+func (l *ZapLogger) WithLayer(layer string) Logger {
+    return &ZapLogger{
+        logger: l.logger.With("layer", layer),
+        layer:  layer,
+    }
+}
 
-	logger, _ := config.Build()
-	return &ZapLogger{
-		logger: logger.Sugar(),
-	}
+// WithField adds a custom field to the logger
+func (l *ZapLogger) WithField(key string, value interface{}) Logger {
+    return &ZapLogger{
+        logger: l.logger.With(key, value),
+        layer:  l.layer,
+    }
 }
 
 func (l *ZapLogger) Info(args ...any) {
-	l.logger.Info(args...)
+    l.logger.Info(args...)
 }
 
 func (l *ZapLogger) Infof(format string, args ...any) {
-	l.logger.Infof(format, args...)
+    l.logger.Infof(format, args...)
 }
 
 func (l *ZapLogger) Debug(args ...any) {
-	l.logger.Debug(args...)
+    l.logger.Debug(args...)
 }
 
 func (l *ZapLogger) Debugf(format string, args ...any) {
-	l.logger.Debugf(format, args...)
+    l.logger.Debugf(format, args...)
 }
 
 func (l *ZapLogger) Error(args ...any) {
-	l.logger.Error(args...)
+    l.logger.Error(args...)
 }
 
 func (l *ZapLogger) Errorf(format string, args ...any) {
-	l.logger.Errorf(format, args...)
+    l.logger.Errorf(format, args...)
 }
 
 func (l *ZapLogger) Fatal(args ...any) {
-	l.logger.Fatal(args...)
+    l.logger.Fatal(args...)
 }
 
 func (l *ZapLogger) Fatalf(format string, args ...any) {
-	l.logger.Fatalf(format, args...)
+    l.logger.Fatalf(format, args...)
 }
 
 func (l *ZapLogger) Warn(args ...any) {
-	l.logger.Warn(args...)
+    l.logger.Warn(args...)
 }
 
 func (l *ZapLogger) Warnf(format string, args ...any) {
-	l.logger.Warnf(format, args...)
+    l.logger.Warnf(format, args...)
 }
